acb8f154b61ad1defa5d746e397f753a7d0281d0
linyyy <1398809407@qq.com>
2024-03-14 17:46:22 +0800

    push

--- src/components/sideMenu.tsx
+++ src/components/sideMenu.tsx
@@ -1,15 +1,19 @@
-import { routes } from "@/router";
+import { routes } from "@/router/route";
 import { Link } from "react-router-dom";
 function SideMenuItem({ path, label }: { path: string, label: string }) {
   return (
-    <div>
+    <div className="hover:bg-blue-100 p-2 m-2 w-20 h-10 cursor-pointer text-center transition rounded-md">
       <Link to={path}>{label}</Link>
     </div>
   )
 }
 
+
 function SideMenu() {
   const nodes: JSX.Element[] = []
+  const click = () => {
+
+  }
   routes.forEach(item => {
     nodes.push(<SideMenuItem path={item.path} label={item.handle} />)
   })
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './tailwindcss.css'
 import { Provider } from 'react-redux'
 import store from './store'
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
-
+import './tailwindcss.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
